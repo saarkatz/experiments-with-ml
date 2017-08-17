2eaa1a49433e5f37c0ee4cae602d1b7e2503de4f
Neta Friedman <neta98@gmail.com>
2017-08-17 16:14:46 +0300

    cost function added

--- src/NNFunctions.py
+++ src/NNFunctions.py
@@ -8,13 +8,15 @@ def init_weight (l_in, l_out):
     weights = np.random.rand(l_out, 1 + l_in) * 2 * epsilon_init - epsilon_init
     return weights
 
-def back_prop (output, exp_out):
+def back_prop (, ):
     return
 
-def cost_function(self, data_set):
+# Function gets set of training examples and returns the cost function
+def cost_function(nn, data_set):
     j = 0
-    for input, output in data_set:
-        for k in input.size:
-            j += -output[k]*np.log(run(input)[k]) + (1-output[k])*np.log(run(input)[k])
+    for input_vec, output in data_set:
+        run_res = nn.run(input_vec)
+        for k in range(input_vec.size):
+            j += -output[k]*np.log(run_res[k]) + (1-output[k])*np.log(run_res[k])
     j *= (1/len(data_set))
     return j

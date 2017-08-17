2ad08e9e704cf564fe6a7ec55c8ce0c66ef91a2f
Neta Friedman <neta98@gmail.com>
2017-08-17 14:19:49 +0300

    Random Weights Init added

---
+++ src/randInitWeight.py
@@ -0,0 +1,10 @@
+import numpy as np
+#Function inputs are:
+#l_in: size of the input layer for the current layer.
+#l_out: size of the output layer for the current layer.
+#returns random weights matrix
+def init_weight (l_in, l_out):
+    epsilon_init = np.sqrt(6) / (sqrt(l_in + l_out))
+    weights = np.random.rand(l_out, 1 + l_in) * 2 * epsilon_init - epsilon_init
+    return weights
+

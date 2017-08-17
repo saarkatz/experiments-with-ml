6eb001f351b6d53038d7cf9c7ff1a8ef3aaf40b3
Saar Katz <kats.saar@gmail.com>
2017-08-17 17:39:41 +0300

    Fixed run and _run_all_partial
    Added create_const

--- src/NeuralNetwork.py
+++ src/NeuralNetwork.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.special import expit
 
 #Function inputs are:
 #l_in: size of the input layer for the current layer.
@@ -33,20 +34,27 @@ class NeuralNetwork:
         # Get the result of the previous layer
         input_vector = self._prev_layer.run(input_dict)
 
+        # Concatenate bias value
+        full_input_vector = np.concatenate((np.ones((1,)), input_vector))
+
         # Calculate and return the out vector
-        return np.dot(self._matrix, input_vector)
+        return expit(np.dot(self._matrix, full_input_vector))
 
     def _run_all_partial(self, input_dict):
         if self.is_placeholder:
             # Return the values given in input_dict
-            return [input_dict[self.name]]
+            return [input_dict[self.name]], [input_dict[self.name]]
 
         # Get the result of the previous layer
-        history = self._prev_layer.run(input_dict)
+        history_z, history_a = self._prev_layer._run_all_partial(input_dict)
+
+        # Concatenate bias value
+        full_input_vector = np.concatenate((np.ones((1,)), history_a[-1]))
 
         # Calculate and return the out vector along with the rest of the history
-        history.append(np.dot(self._matrix, history[-1]))
-        return history
+        history_z.insert(0, np.dot(self._matrix, full_input_vector))
+        history_a.insert(0, expit(history_z[-1]))
+        return history_z, history_a
 
 
 def create_placeholder(name, size=1):
@@ -56,5 +64,41 @@ def create_placeholder(name, size=1):
 def create_dense_layer(name, size, input_layer):
     return NeuralNetwork(name, size, input_layer, False)
 
-def create_const(name, size):
-    pass
+
+def create_const(name, size, const):
+    nn = NeuralNetwork(name, size, None, False)
+    nn.run = lambda x: np.ones((size,)) * const
+    nn._run_all_partial = lambda x: [np.ones((size,)) * const], [np.ones((size,)) * const]
+    return nn
+
+def add_networks(network1, network2):
+    nn = NeuralNetwork('+', network1.size, None, False)
+    def run(input_dict):
+        # Get the result of the previous layer
+        input_vector1 = network1.run(input_dict)
+        input_vector2 = network2.run(input_dict)
+
+        return input_vector1 + input_vector2
+
+    def _run_all_partial(input_dict):
+        history_z_1, history_a_1 = network1._run_all_partial(input_dict)
+        history_z_2, history_a_2 = network2._run_all_partial(input_dict)
+
+        out_vector = history_a_1[-1] + history_a_2[-1]
+        history_z_1.extend(history_z_2)
+        history_a_1.extend(history_a_2)
+        history_z_1.append(out_vector)
+        history_a_1.append(out_vector)
+
+        return history_z_1, history_a_1
+
+    nn.run = run
+    nn._run_all_partial = _run_all_partial
+    return nn
+
+
+if __name__ == '__main__':
+    a = create_placeholder('input', 1)
+    b = create_dense_layer('b', 1, a)
+
+    print(b._run_all_partial({'input': np.array([2])}))

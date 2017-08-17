24a2f4179900caa92a8181eeeea0c5cf35d9cc0e
Neta Friedman <neta98@gmail.com>
2017-08-17 14:26:56 +0300

    Random Weights Init added

--- src/NeuralNetwork.py
+++ src/NeuralNetwork.py
@@ -1,5 +1,14 @@
 import numpy as np
 
+#Function inputs are:
+#l_in: size of the input layer for the current layer.
+#l_out: size of the output layer for the current layer.
+#returns random weights matrix
+def init_weight (l_in, l_out):
+    epsilon_init = np.sqrt(6) / (np.sqrt(l_in + l_out))
+    weights = np.random.rand(l_out, 1 + l_in) * 2 * epsilon_init - epsilon_init
+    return weights
+
 class NeuralNetwork:
     def __init__(self, name, size, input_layer=None, is_placeholder=False):
         self.name = name
--- src/randInitWeight.py
+++ src/randInitWeight.py
@@ -4,7 +4,7 @@ import numpy as np
 #l_out: size of the output layer for the current layer.
 #returns random weights matrix
 def init_weight (l_in, l_out):
-    epsilon_init = np.sqrt(6) / (sqrt(l_in + l_out))
+    epsilon_init = np.sqrt(6) / (np.sqrt(l_in + l_out))
     weights = np.random.rand(l_out, 1 + l_in) * 2 * epsilon_init - epsilon_init
     return weights
 

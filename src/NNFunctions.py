dbc14d778c1115f735bb7062cfb914b7ccb97f53
Neta Friedman <neta98@gmail.com>
2017-08-17 17:42:11 +0300

    cost function added

--- src/NNFunctions.py
+++ src/NNFunctions.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.special import expit
 #Function inputs are:
 #l_in: size of the input layer for the current layer.
 #l_out: size of the output layer for the current layer.
@@ -8,8 +9,21 @@ def init_weight (l_in, l_out):
     weights = np.random.rand(l_out, 1 + l_in) * 2 * epsilon_init - epsilon_init
     return weights
 
-def back_prop (, ):
-    return
+def back_prop (self, input_dic):
+    a, z = _run_all_partial(input_dic)
+    lambda_vec = [None] * (len(a)-1)
+    delta_vec = [None] * (len(a)-1)
+    grad_vec = [None] * (len(a)-1)
+    y = np.zeros(len(a[0]))
+    y[np.argmax(a[0])] = 1
+    lambda_vec[0] = np.array(a[0]) - y
+    current_layer = self._prev_layer
+    for k in range(lambda_vec[1:].size):
+        lambda_vec[k+1] = np.multiply((lambda_vec[k]*current_layer._matrix), np.multiply(expit(z[k+1]), 1-expit(z[k+1])))
+        lambda_vec[k+1] = lambda_vec[k+1][1:]
+        delta_vec[k] = lambda_vec[k+1]*np.matrix.transpose(a[k])
+        grad_vec[k] = (1 / len(input_dic)) * delta_vec[k]
+    return grad_vec
 
 # Function gets set of training examples and returns the cost function
 def cost_function(nn, data_set):
